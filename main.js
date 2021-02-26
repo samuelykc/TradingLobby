7228dd8c03083335ba015be84f57d0e0ec96d20c
samuelykc <23565627+samuelykc@users.noreply.github.com>
2021-02-27 05:02:53 +0900

    Electron setup

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+
+
+
+// ipcMain.handle('perform-action', (event, ...args) => {
+//   // ... do actions on behalf of the Renderer
+// })
